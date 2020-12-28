bf2e4eff0937e7a80e6defab586a759df435d588
jiachengyu <jiacywork@outlook.com>
2020-12-28 16:14:55 +0800

    提交爬虫信息

---
+++ ShopSpider/tools/LoggingFormat.py
@@ -0,0 +1,18 @@
+import logging
+import datetime as dt
+
+
+class MyFormatter(logging.Formatter):
+    """
+    重写日期格式
+    """
+    converter = dt.datetime.fromtimestamp
+
+    def formatTime(self, record, datefmt=None):
+        ct = self.converter(record.created)
+        if datefmt:
+            s = ct.strftime(datefmt)
+        else:
+            t = ct.strftime("%Y-%m-%d %H:%M:%S")
+            s = "%s,%03d" % (t, record.msecs)
+        return s
